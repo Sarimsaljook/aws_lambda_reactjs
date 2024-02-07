f7e15ce9dad7b6c1d1e8194e313ab1a69941386d
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2024-02-07 16:18:16 +0500

    Add UID valindation feature

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
+import axios from 'axios';
 import './App.css';
 import React, { useState } from 'react';
-import axios from "axios";
 
 function App() {
   const [userID, setUserID] = useState(""); 
 
-  const fetchDataWithId = async () => {
-      await axios.get(`https://n9cj0xxi0f.execute-api.us-east-1.amazonaws.com/test/`).then((response) => {
-    console.log(response);
-  }).catch((err) => console.error('Error fetching data:', err));
+  const fetchDataWithId = () => { 
+
+        axios.post('https://n9cj0xxi0f.execute-api.us-east-1.amazonaws.com/primary/', {
+          "routeKey": "POST /",
+          "pathParameters": {
+            "id": userID
+          }
+      }).then((res) => {
+
+        // Assuming res.data holds the string
+        const resData = res.data;
+
+        // Find the start index of the "body" field
+        const bodyStartIndex = resData.indexOf('{"id":"') + '{"id":"'.length;
+
+        // Find the end index of the "body" field
+        const bodyEndIndex = resData.indexOf('"}"', bodyStartIndex);
+
+        // Extract the "body" field substring
+        const bodySubstring = resData.substring(bodyStartIndex, bodyEndIndex);
+
+        if (bodySubstring === userID) { 
+          alert("User Found ✅!");
+          // Display the extracted body substring
+          console.log('User' + bodySubstring + ' found!');
+
+          setUserID("");
+      } else {
+         alert("No User Found With The ID "+userID+" ❌");
+         console.log('User' + bodySubstring + 'not found!');
+
+         setUserID("");
+         }
+      
+      }).catch((err) => console.log(err));
+
   };
 
   return (
-   <div className="App">
+    <div className="App">
       <h1>Validate Your ID:</h1>
-         <input value={userID} onChange={(e) => setUserID(e.target.value)} />
-         <button onClick={fetchDataWithId}>Validate</button>
+      <input value={userID} onChange={(e) => setUserID(e.target.value)} />
+      <button style={{ marginLeft: 10 }} onClick={fetchDataWithId}>Validate</button>
     </div>
   );
 }
